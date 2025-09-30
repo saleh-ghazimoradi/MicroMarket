8658c37ad609cc5bad97cd92604bf0e055de55b6
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-09-30 17:22:54 +0330

    implement the grpc client and server

---
+++ account/gateway/grpcHandler/client.go
@@ -0,0 +1,66 @@
+package grpcHandler
+
+import (
+	"context"
+	"github.com/saleh-ghazimoradi/MicroMarket/account/domain"
+	"github.com/saleh-ghazimoradi/MicroMarket/account/gateway/proto"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type GRPCAccountClient struct {
+	conn   *grpc.ClientConn
+	client proto.AccountServiceClient
+}
+
+func (g *GRPCAccountClient) CreateAccount(ctx context.Context, name string) (*domain.Account, error) {
+	resp, err := g.client.CreateAccount(ctx, &proto.CreateAccountRequest{Name: name})
+	if err != nil {
+		return nil, err
+	}
+	return &domain.Account{
+		Id:   resp.Account.Id,
+		Name: name,
+	}, nil
+}
+
+func (g *GRPCAccountClient) GetAccountById(ctx context.Context, id string) (*domain.Account, error) {
+	resp, err := g.client.GetAccountById(ctx, &proto.GetAccountRequest{Id: id})
+	if err != nil {
+		return nil, err
+	}
+	return &domain.Account{
+		Id:   resp.Account.Id,
+		Name: resp.Account.Name,
+	}, nil
+}
+
+func (g *GRPCAccountClient) GetAccounts(ctx context.Context, offset, limit uint64) ([]*domain.Account, error) {
+	resp, err := g.client.GetAccounts(ctx, &proto.GetAccountsRequest{Offset: offset, Limit: limit})
+	if err != nil {
+		return nil, err
+	}
+	accounts := make([]*domain.Account, len(resp.Accounts))
+	for _, account := range resp.Accounts {
+		accounts = append(accounts, &domain.Account{
+			Id:   account.Id,
+			Name: account.Name,
+		})
+	}
+	return accounts, nil
+}
+
+func (g *GRPCAccountClient) Close() error {
+	return g.conn.Close()
+}
+
+func NewGRPCAccountClient(addr string) (*GRPCAccountClient, error) {
+	client, err := grpc.NewClient(addr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, err
+	}
+	accountServiceClient := proto.NewAccountServiceClient(client)
+	return &GRPCAccountClient{
+		client: accountServiceClient,
+	}, nil
+}
---
+++ account/gateway/grpcHandler/server.go
@@ -0,0 +1,69 @@
+package grpcHandler
+
+import (
+	"context"
+	"github.com/saleh-ghazimoradi/MicroMarket/account/gateway/proto"
+	"github.com/saleh-ghazimoradi/MicroMarket/account/service"
+	"google.golang.org/grpc"
+	"net"
+)
+
+type GRPCHandler struct {
+	accountService service.AccountService
+	proto.UnimplementedAccountServiceServer
+}
+
+func (g *GRPCHandler) CreateAccount(ctx context.Context, r *proto.CreateAccountRequest) (*proto.CreateAccountResponse, error) {
+	account, err := g.accountService.CreateAccount(ctx, r.Name)
+	if err != nil {
+		return nil, err
+	}
+	return &proto.CreateAccountResponse{
+		Account: &proto.Account{
+			Id:   account.Id,
+			Name: account.Name,
+		},
+	}, nil
+}
+
+func (g *GRPCHandler) GetAccountById(ctx context.Context, r *proto.GetAccountRequest) (*proto.GetAccountResponse, error) {
+	account, err := g.accountService.GetAccount(ctx, r.Id)
+	if err != nil {
+		return nil, err
+	}
+	return &proto.GetAccountResponse{
+		Account: &proto.Account{
+			Id:   account.Id,
+			Name: account.Name,
+		},
+	}, nil
+}
+
+func (g *GRPCHandler) GetAccounts(ctx context.Context, r *proto.GetAccountsRequest) (*proto.GetAccountsResponse, error) {
+	acc, err := g.accountService.GetAccounts(ctx, r.Offset, r.Limit)
+	if err != nil {
+		return nil, err
+	}
+	accounts := make([]*proto.Account, 0, len(acc))
+	for _, ac := range acc {
+		accounts = append(accounts, &proto.Account{
+			Id:   ac.Id,
+			Name: ac.Name,
+		})
+	}
+	return &proto.GetAccountsResponse{Accounts: accounts}, nil
+}
+
+func (g *GRPCHandler) Serve(addr string) error {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		return err
+	}
+	grpcServer := grpc.NewServer()
+	proto.RegisterAccountServiceServer(grpcServer, g)
+	return grpcServer.Serve(lis)
+}
+
+func NewGRPCHandler(accountService service.AccountService) *GRPCHandler {
+	return &GRPCHandler{accountService: accountService}
+}
