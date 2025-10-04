1ee2204b5a473839720242f52b6a1773792fe582
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-10-04 13:24:54 +0330

    fix a typo in my postgresql script

--- order/migrations/0002_create_order_catalog_table.up.sql
+++ order/migrations/0002_create_order_catalog_table.up.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS order_catalog (
-    order_id CHAR(27) REFERENCES orders (id) ON DELETE CASCADE,
+    order_id CHAR(27) REFERENCES order (id) ON DELETE CASCADE,
     catalog_id CHAR(27),
     quantity INT NOT NULL,
     PRIMARY KEY (catalog_id, order_id)
