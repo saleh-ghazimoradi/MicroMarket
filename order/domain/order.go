33f23d8bb118a003014ace5809e38c4c8287fbd8
Saleh Ghazimoradi <saleh.d.h.ghazimoradi@gmail.com>
2025-10-04 13:26:05 +0330

    define orderCatalog domain

---
+++ order/domain/order.go
@@ -0,0 +1,19 @@
+package domain
+
+import "time"
+
+type Order struct {
+	Id         string           `json:"id"`
+	CreatedAt  time.Time        `json:"created_at"`
+	TotalPrice float64          `json:"total_price"`
+	AccountId  string           `json:"account_id"`
+	Catalogs   []OrderedCatalog `json:"catalog"`
+}
+
+type OrderedCatalog struct {
+	Id          string  `json:"id"`
+	Name        string  `json:"name"`
+	Description string  `json:"description"`
+	Price       float64 `json:"price"`
+	Quantity    uint32  `json:"quantity"`
+}
